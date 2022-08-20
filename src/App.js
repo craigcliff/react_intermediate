73b3803d53342843549985a1e6d4edb3d7eccd66
craigcliff <craigcliff@gmail.com>
2022-08-20 10:11:36 +0200

    Added layout basics

--- src/App.js
+++ src/App.js
@@ -18,8 +18,10 @@ const App = () => {
           }}
         >
           <BrowserRouter>
-            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
-              <Link to="/" className="text-6xl text-white hover:text-gray-200">Adopt Me!</Link>
+            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b
+             from-purple-400 via-cyan-500 to-red-500">
+              <Link to="/" className="text-6xl text-white 
+              hover:text-gray-200">Adopt Me!</Link>
             </header>
             <Routes>
               <Route path="/details/:id" element={<Details />} />
