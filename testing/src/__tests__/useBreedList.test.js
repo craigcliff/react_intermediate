acb966a8557cc74618f891825cdf4a42739c2d7d
craigcliff <craigcliff@gmail.com>
2022-08-25 20:13:15 +0200

    Testing custom hook without react hook testing

---
+++ testing/src/__tests__/useBreedList.test.js
@@ -0,0 +1,27 @@
+/**
+ *  @jest-environment jsdom
+ */
+
+ import { expect, test } from '@jest/globals';
+ import { render } from '@testing-library/react';
+ import { StaticRouter } from 'react-router-dom/server'
+ import useBreedList from '../useBreedList';
+ 
+function getBreedList(animal) {
+    let list;
+
+    function TestComponent() {
+        list = useBreedList(animal);
+        return null;
+    }
+
+    render(<TestComponent />);
+
+    return list;
+}
+
+test ("gives an empty list with no animal", async () => {
+    const [breedList, status] = getBreedList();
+    expect(breedList).toHaveLength(0);
+    expect(status).toBe("unloaded");
+})
