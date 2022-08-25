433b213aaa8c520866e33aed50cd3a5deaeb7e69
craigcliff <craigcliff@gmail.com>
2022-08-25 20:23:41 +0200

    Added mocks

--- testing/src/__tests__/useBreedList.test.js
+++ testing/src/__tests__/useBreedList.test.js
@@ -8,9 +8,37 @@
  
 
 
-test ("gives an empty list with no animal", async () => {
+test("gives an empty list with no animal", async () => {
     const { result } = renderHook(() => useBreedList(""));
     const [breedList, status] = result.current;
     expect(breedList).toHaveLength(0);
     expect(status).toBe("unloaded");
+})
+
+test ("gives back breeds with an animal", async () => {
+    const breeds = [
+        "Havanese",
+        "Bichon Frise",
+        "Poodle",
+        "Maltese",
+        "Golden Retriever",
+        "Labrador",
+        "Husky"
+    ];
+
+    fetch.mockResponseOnce(
+        JSON.stringify({
+            animal: "dog",
+            breeds
+        })
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() => useBreedList("dog"))
+
+    await waitForNextUpdate();
+
+    const [breedList, status] = result.current;
+    expect(status).toBe("loaded");
+    expect(breedList).toEqual(breeds);
+
 })
